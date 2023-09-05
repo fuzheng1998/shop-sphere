7bf1df7761d1ccb77b3e235881863e965b212cb4
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-09-05 18:00:26 +1000

    put product box component into home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  cols = 3;
+  rowHeight: number = ROWS_HEIGHT[this.cols];
 }
