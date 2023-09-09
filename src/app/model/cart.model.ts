1738faface76a16395eaa05da80db0006e6bc8cd
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-09-10 09:44:46 +1000

    display cart as table

--- src/app/model/cart.model.ts
+++ src/app/model/cart.model.ts
@@ -1,10 +1,3 @@
-export interface Cart {
-  items: Array<CartItem>;
-
-
-
-}
-
 export interface CartItem {
   product: string;
   name: string;
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,25 +1,43 @@
 import {Component} from '@angular/core';
-import {Cart, CartItem} from "../../model/cart.model";
+import {CartItem} from "../../model/cart.model";
+
+
+const ELEMENT_DATA: Array<CartItem> = [
+  {
+    product: '1',
+    name: 'Product 1',
+    price: 100,
+    quantity: 1,
+    id: 1,
+  },
+  {
+    product: '2',
+    name: 'Product 2',
+    price: 200,
+    quantity: 2,
+    id: 2,
+  },
+  {
+    product: '3',
+    name: 'Product 3',
+    price: 300,
+    quantity: 3,
+    id: 3,
+  }
+];
 
 @Component({
-    selector: 'app-cart',
-    templateUrl: './cart.component.html',
-    styleUrls: ['./cart.component.css'],
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-    cart: Cart = {
-        items: [
-            {product: 'product1', name: 'name1', price: 1, quantity: 1, id: 1},
-            {product: 'product2', name: 'name2', price: 2, quantity: 2, id: 2},
-            {product: 'product3', name: 'name3', price: 3, quantity: 3, id: 3},
-        ]
-    };
-    dataSources: Array<CartItem> = []
-
-    ngOnInit() {
-        this.dataSources = this.cart.items;
-    }
-
-    displayedColumns: string[] = ['product', 'name', 'price', 'quantity', 'id'];
+  displayedColumns: string[] = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+  ];
+  dataSource = ELEMENT_DATA;
 
 }
