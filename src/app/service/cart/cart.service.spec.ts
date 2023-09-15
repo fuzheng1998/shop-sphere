13d25f431d49d7ccbd81d688b084492909bb5a63
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-09-15 14:23:28 +1000

    add cart service test

--- src/app/service/cart/cart.service.spec.ts
+++ src/app/service/cart/cart.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { CartService } from './cart.service';
+import {CartService} from './cart.service';
 
 describe('CartService', () => {
   let service: CartService;
@@ -12,5 +12,48 @@ describe('CartService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add item to cart', () => {
+    const item = {
+      product: 'https://placehold.co/150',
+      name: 'Product 4',
+      price: 400,
+      quantity: 4,
+      id: 4,
+    };
+    service.addToCart(item);
+    expect(service.cart.value.length).toBe(4);
+  });
+
+  it('should remove item from cart', () => {
+    const item = {
+      product: 'https://placehold.co/150',
+      name: 'Product 4',
+      price: 400,
+      quantity: 4,
+      id: 4,
+    };
+    service.addToCart(item);
+    service.removeFromCart(item);
+    expect(service.cart.value.length).toBe(3);
+  });
+
+  it('should clear cart', () => {
+    service.clearCart();
+    expect(service.cart.value.length).toBe(0);
+  });
+
+  it('should remove quantity', () => {
+    const item = {
+      product: 'https://placehold.co/150',
+      name: 'Product 4',
+      price: 400,
+      quantity: 4,
+      id: 4,
+    };
+    service.addToCart(item);
+    service.removeQuantity(item);
+    expect(service.cart.value[3].quantity).toBe(3);
   });
 });
